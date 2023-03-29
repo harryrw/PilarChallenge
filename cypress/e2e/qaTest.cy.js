a7be4f975d45b74352c40ce2153bd4376e928e19
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-29 14:47:52 +1100

    Fix "CORS script error" by ignoring uncaught exception

--- cypress/e2e/qaTest.cy.js
+++ cypress/e2e/qaTest.cy.js
@@ -1,14 +1,15 @@
-describe('Download file & validate', () => {
-    it('Visit, verify download, open file', () => {
-        // Visit page
-        cy.visit('https://demoqa.com/upload-download')
-        // Click download
-        cy.get('#downloadButton')
-        .click()
-        // Verify file downloads in cypress project download folder
-        cy.readFile('cypress/downloads/sampleFile.jpeg')
-        // Open downloaded file
-        // Verify file opens correctly with no errors
-        // Make sure project runs headless with Chrome
-    })
-})
+describe("Download file & validate", () => {
+  it("Visit, verify download, open file", () => {
+    // Ignore uncaught exception on page load to fix CORS error
+    cy.once("uncaught:exception", () => false);
+    // Visit page
+    cy.visit("/");
+    // Click download
+    cy.get("#downloadButton").click({ force: true });
+    // Verify file downloads in cypress project download folder
+    cy.readFile("cypress/downloads/sampleFile.jpeg");
+    // Open downloaded file
+    // Verify file opens correctly with no errors
+    // Make sure project runs headless with Chrome
+  });
+});
